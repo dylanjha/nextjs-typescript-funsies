8539d7feb90c2c996c5efe00d397b25122c7fcdc
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2021-08-17 12:52:49 -0700

    add a route for /mux-video

---
+++ pages/mux-video.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+
+  useEffect(() => {
+    //
+    // import this client side b/c ES modules on the server are no-bueno
+    // https://github.com/vercel/next.js/issues/9890
+    //
+    import('@mux/mux-video');
+  }, [])
+
+  return (
+    <div>
+      <Head>
+        <title>mux-video element</title>
+      </Head>
+
+      <main>
+        <mux-video
+          playback-id="DS00Spx1CV902MCtPj5WknGlR102V5HFkDe"
+          controls
+          autoplay
+          muted
+          debug
+          env-key="cqtqt2jfbq235huvso0djbn56"
+        >
+        </mux-video>
+
+      </main>
+      <style jsx>{`
+        main {
+          padding: 60px;
+        }
+      `}
+      </style>
+    </div>
+  )
+}
+
+export default Home
